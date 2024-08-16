8ab4dda752edc9873391237c10d2b66981cacceb
Nick Rebezniuk <mkla.rbzk@gmail.com>
2024-08-16 15:33:17 -0400

    Salary update

--- home/templatetags/income.py
+++ home/templatetags/income.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter
 def income(td):
     total_seconds = int(td.total_seconds())
-    rate_per_second = 950 / 3600
+    rate_per_second = 1000 / 3600
     total = total_seconds * rate_per_second
     dollars = math.floor(total / 100)
     cents = math.floor(total % 100)
