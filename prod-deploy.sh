003bed2008f3eddb9a21d0cf609d3a50244f4f19
Nick Rebezniuk <mkla.rbzk@gmail.com>
2025-01-11 21:16:02 -0500

    Deployment automation update.

--- prod-deploy.sh
+++ prod-deploy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-git pull;
-docker compose -f docker-compose.prod.yml build --push;
-docker stack deploy --compose-file docker-compose.prod.yml rbzk_pro;
+if git pull; then
+    echo -e "\e[1;32m PULL complete \e[0m"
+else
+    echo -e "\e[1;31m PULL failed \e[0m"
+    exit;
+fi
+
+
+if  docker compose -f docker-compose.prod.yml build --push; then
+    echo -e "\e[1;32m Docker images BUILD SUCCESS \e[0m"
+else
+    echo -e "\e[1;31m Docker images BUILD FAILED \e[0m"
+    exit;
+fi
+
+if  docker stack deploy --compose-file docker-compose.prod.yml rbzk_pro; then
+    echo -e "\e[1;32m Stack UPDATE SUCCESS \e[0m"
+else
+    echo -e "\e[1;31m STACK UPDATE FAILED \e[0m"
+    exit;
+fi
+
 
 
