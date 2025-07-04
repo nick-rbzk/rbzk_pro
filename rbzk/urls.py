4cfcd33133fa945de1d28bf6aa697ad344eff7e2
Nick Rebezniuk <mkla.rbzk@gmail.com>
2025-07-03 20:41:03 -0400

    Calendar Statistics and performance app for GCal.

--- rbzk/urls.py
+++ rbzk/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('89ee14c8c7cb465faa10ec1a5e142fd3/', admin.site.urls),
-    path('', include("home.urls"))
+    path('', include("home.urls")),
+    path('', include("cal_stats.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
