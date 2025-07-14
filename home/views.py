f171b3866c068c6cb41373a195d05e1eab97ebf9
Nick Rebezniuk <mkla.rbzk@gmail.com>
2025-07-14 15:15:41 -0400

    Jobs page accessable only when admin is logged in.

--- home/views.py
+++ home/views.py
@@ -63,44 +63,47 @@ def contact_api(request):
 
 @never_cache
 def jobs_page(request):
-    context = {}
-    context['form'] = JobForm()
-    context["weeks"] = WorkWeek.objects.all().exclude(jobs_time__isnull=True)
-    context["is_logged_in"] = request.user.is_authenticated
-    if request.method == "POST":
-        data = request.POST
-        job_start_date = data.get("job_start_date")
-        job_start_time= data.get("job_start_time")
-        job_end_date= data.get("job_end_date")
-        job_end_time = data.get("job_end_time")
-        confirmation = data.get("confirmation")
-        notes = data.get("notes")
-        
-        job_start = job_start_date.strip() + " " + job_start_time.strip()
-        job_end = job_end_date.strip() + " " + job_end_time.strip()
-        job_start_datetime = timezone.make_aware(datetime.strptime(job_start, '%m/%d/%Y %H:%M'))
-        job_end_datetime = timezone.make_aware(datetime.strptime(job_end, '%m/%d/%Y %H:%M'))
+    if request.user.is_authenticated:
+        context = {}
+        context['form'] = JobForm()
+        context["weeks"] = WorkWeek.objects.all().exclude(jobs_time__isnull=True)
+        context["is_logged_in"] = request.user.is_authenticated
+        if request.method == "POST":
+            data = request.POST
+            job_start_date = data.get("job_start_date")
+            job_start_time= data.get("job_start_time")
+            job_end_date= data.get("job_end_date")
+            job_end_time = data.get("job_end_time")
+            confirmation = data.get("confirmation")
+            notes = data.get("notes")
+            
+            job_start = job_start_date.strip() + " " + job_start_time.strip()
+            job_end = job_end_date.strip() + " " + job_end_time.strip()
+            job_start_datetime = timezone.make_aware(datetime.strptime(job_start, '%m/%d/%Y %H:%M'))
+            job_end_datetime = timezone.make_aware(datetime.strptime(job_end, '%m/%d/%Y %H:%M'))
 
-        week_start_datetime, week_end_datetime = which_week(job_end_datetime)
-        if isinstance(week_start_datetime, datetime) and isinstance(week_end_datetime, datetime):
-            week, created = WorkWeek.objects.get_or_create(
-                week_start=week_start_datetime, 
-                week_end=week_end_datetime
-            )
-            if not created:
-                week.save()
-            # add a job to that week
-            job, created = ParkJob.objects.get_or_create(
-                job_start=job_start_datetime,
-                job_end=job_end_datetime,
-                confirmation=confirmation,
-                notes=notes,
-                workweek=week,
-            )
-            if not created:
-                job.save()
-        context["weeks"] = WorkWeek.objects.all()
-        context["form"] = JobForm()
-        return render(request, "jobs.html", context)
+            week_start_datetime, week_end_datetime = which_week(job_end_datetime)
+            if isinstance(week_start_datetime, datetime) and isinstance(week_end_datetime, datetime):
+                week, created = WorkWeek.objects.get_or_create(
+                    week_start=week_start_datetime, 
+                    week_end=week_end_datetime
+                )
+                if not created:
+                    week.save()
+                # add a job to that week
+                job, created = ParkJob.objects.get_or_create(
+                    job_start=job_start_datetime,
+                    job_end=job_end_datetime,
+                    confirmation=confirmation,
+                    notes=notes,
+                    workweek=week,
+                )
+                if not created:
+                    job.save()
+            context["weeks"] = WorkWeek.objects.all()
+            context["form"] = JobForm()
+            return render(request, "jobs.html", context)
+        else:
+            return render(request, "jobs.html", context)
     else:
-        return render(request, "jobs.html", context)
+        return redirect("home_page")
