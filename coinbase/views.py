035267158e7b0d476835baf824c8f133d3f8b7cc
Nick Rebezniuk <mkla.rbzk@gmail.com>
2026-03-05 16:32:49 -0500

    new app files.

---
+++ coinbase/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# 20 day high
+# 20 day low
+
+# every day collect the highest hight and the lowest low. for 20 days, every 21st day remove the first day
