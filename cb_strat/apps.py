d5d7c983d6427ee9893c04623136a3842b9ef1fa
Nick Rebezniuk <mkla.rbzk@gmail.com>
2026-03-27 10:32:06 -0400

    Old app removed.

--- cb_strat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CbStratConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cb_strat'
--- cb_strat/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class ActiveTask(models.Model):
-    task_id = models.CharField(max_length=256, blank=False, null=True)
-    name = models.CharField(max_length=1024, blank=False, null=True)
-    
