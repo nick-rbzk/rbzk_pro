8a18fdec65d5016f3ff0ef32374d9f164e9f01af
Nick Rebezniuk <mkla.rbzk@gmail.com>
2026-04-09 12:32:36 -0400

    Spotter job filtering options

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 from .models import *
 # Register your models here.
 
@@ -7,9 +8,22 @@ admin.site.register(FormSubmission)
 
 @admin.register(ParkJob)
 class ParkJobAdmin(admin.ModelAdmin):
-    list_display    = ("confirmation", "total", "job_start", "job_end")
+    list_display    = ("confirmation", "total", "jb_start", "jb_end")
+    list_filter     = ("job_start", "job_end")
     search_fields   = ("confirmation", "job_start", "job_end")
 
+    def jb_start(self, obj):
+        return f"{timezone.localtime(obj.job_start):%m/%d/%Y %I:%M %p}"
+    
+    jb_start.admin_order_field = 'timefield'
+    jb_start.short_description = 'Job Start'  
+    
+    def jb_end(self, obj):
+        return f"{timezone.localtime(obj.job_end):%m/%d/%Y %I:%M %p}"
+    
+    jb_end.admin_order_field = 'timefield'
+    jb_end.short_description = 'Job End'
+ 
     def total(self, obj):
         return f"{obj.job_income()}"
     
