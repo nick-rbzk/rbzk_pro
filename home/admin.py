29468a7b6f7e6bd4a73a3f47451388a9a07b1f7e
Nick Rebezniuk <mkla.rbzk@gmail.com>
2026-04-09 12:19:58 -0400

    Spotter jobs admin update.

--- home/admin.py
+++ home/admin.py
@@ -3,5 +3,14 @@ from .models import *
 # Register your models here.
 
 admin.site.register(WorkWeek)
-admin.site.register(ParkJob)
 admin.site.register(FormSubmission)
+
+@admin.register(ParkJob)
+class ParkJobAdmin(admin.ModelAdmin):
+    list_display    = ("confirmation", "total", "job_start", "job_end")
+    search_fields   = ("confirmation", "job_start", "job_end")
+
+    def total(self, obj):
+        return f"{obj.job_income()}"
+    
+    total.short_desctioption = "Total Income"
