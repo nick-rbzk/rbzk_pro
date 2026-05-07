d69a3d8be1f8508ee3e21e6e9e4c2b3e442b0cae
nick <test@yest.com>
2026-05-07 17:20:39 -0400

    Cone hive related final cleanup.

--- home/admin.py
+++ home/admin.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from .models import *
 # Register your models here.
 
-admin.site.register(WorkWeek)
 admin.site.register(FormSubmission)
 
 @admin.register(ParkJob)
@@ -28,3 +27,22 @@ class ParkJobAdmin(admin.ModelAdmin):
         return f"{obj.job_income()}"
     
     total.short_desctioption = "Total Income"
+
+
+@admin.register(WorkWeek)
+class WorkWeekAdmin(admin.ModelAdmin):
+    list_display    = ("week_start", "week_end", "jobs_time", "week_total", "hourly_rate")
+    list_filter     = ("week_start", "week_end")
+    search_fields   = ("jobs_time",)
+
+    def week_total(self, obj):
+        total_seconds = int(obj.jobs_time.total_seconds())
+        rate_per_second = obj.hourly_rate / 3600
+        total = total_seconds * rate_per_second
+        dollars = '{0:.0f}'.format(total / 100)
+        cents = '{0:.0f}'.format(total % 100)
+        return '{}.{}$'.format(dollars, cents)
+    
+    # week_total.admin_order_field = 'timefield'
+    week_total.short_description = 'Week\'s Total'  
+    
--- home/templatetags/income.py
+++ home/templatetags/income.py
@@ -1,13 +1,12 @@
-import math
 from django import template
 register = template.Library()
-
+SECONDS_PER_HOUR = 3600
 @register.filter
 def income(week):
     total_seconds = int(week.jobs_time.total_seconds())
-    rate_per_second = week.hourly_rate / 3600
+    rate_per_second = week.hourly_rate / SECONDS_PER_HOUR
     total = total_seconds * rate_per_second
-    dollars = math.floor(total / 100)
-    cents = math.floor(total % 100)
+    dollars = '{0:.0f}'.format(total / 100)
+    cents = '{0:.0f}'.format(total % 100)
 
     return '{}.{}$'.format(dollars, cents)
