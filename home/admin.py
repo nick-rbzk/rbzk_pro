6d3afec25108082fb0c756846ddd08d82feb5cb2
Nick R. <info@webvision.ltd>
2026-08-02 15:15:18 -0400

    taxes per job.

--- home/admin.py
+++ home/admin.py
@@ -8,7 +8,7 @@ admin.site.register(FormSubmission)
 
 @admin.register(ParkJob)
 class ParkJobAdmin(admin.ModelAdmin):
-    list_display    = ("confirmation", "total", "jb_start", "jb_end")
+    list_display    = ("confirmation", "total", "jb_start", "jb_end", "tax_breakdown", "tax_total")
     list_filter     = ("job_start", "job_end")
     search_fields   = ("confirmation", "job_start", "job_end")
 
@@ -26,9 +26,27 @@ class ParkJobAdmin(admin.ModelAdmin):
  
     def total(self, obj):
         return f"{obj.job_income()}"
+
+    def tax_breakdown(self, obj):
+        income = float(obj.job_income()) 
+        self_employ_owed = self_employ_tax(income)
+        self_employ_owed =  '{0:.j2f}'.format(self_employ_owed)
+        federal_owed = federal_income_tax(income)
+        federal_owed =  '{0:.2f}'.format(federal_owed)
+        return f"Self Employ: {self_employ_owed}$. Federal: {federal_owed}$"
+
+    def tax_total(self, obj):
+        income = float(obj.job_income()) 
+        self_employ_owed = self_employ_tax(income)
+        federal_owed = federal_income_tax(income)
+        total = self_employ_owed + federal_owed
+        total =  '{0:.2f}'.format(total)
+        return f"Total Owed: {total}$"
     
     total.short_desctioption = "Total Income"
-
+    tax_breakdown.short_description = 'Tax breakdown'  
+    tax_total.short_description = 'Total Tax Owed'  
+    
 
 @admin.register(WorkWeek)
 class WorkWeekAdmin(admin.ModelAdmin):
@@ -53,7 +71,7 @@ class WorkWeekAdmin(admin.ModelAdmin):
         income = '{}.{}'.format(dollars, cents)
         income = float(income) 
         self_employ_owed = self_employ_tax(income)
-        self_employ_owed =  '{0:.2f}'.format(self_employ_owed)
+        self_employ_owed =  '{0:.j2f}'.format(self_employ_owed)
         federal_owed = federal_income_tax(income)
         federal_owed =  '{0:.2f}'.format(federal_owed)
         return f"Self Employ: {self_employ_owed}$. Federal: {federal_owed}$"
